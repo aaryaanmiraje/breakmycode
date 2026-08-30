18e486647d2b0aad7d6866e85c384cdf1dc5574f
aaryanmiraje <am6626@srmist.edu.in>
2026-08-30 14:18:37 +0530

    Connect frontend to production API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://breakmycode-api.onrender.com";
 
 const DEFAULT_CODE = ""
 
